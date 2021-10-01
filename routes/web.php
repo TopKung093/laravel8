3f9cb1834db5f92886075ddb6ab9fc9003aa6c29
TopKung093 <86650720+TopKung093@users.noreply.github.com>
2021-10-01 12:55:51 +0700

    Week 13

--- routes/web.php
+++ routes/web.php
@@ -121,7 +121,14 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\OrderProductController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProductController;
-Route::resource('order', OrderController::class);
-Route::resource('payment', PaymentController::class);
-Route::resource('order-product', OrderProductController::class);
+//Route::resource('order', OrderController::class);
+//Route::resource('payment', PaymentController::class);
+//Route::resource('order-product', OrderProductController::class);
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('order', OrderController::class);
+    Route::resource('payment', PaymentController::class);
+    Route::resource('order-product', OrderProductController::class);
+});
 Route::resource('product', ProductController::class);
+
